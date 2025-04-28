11e74267e6e4d13886f8203cb912b9de145236da
James250704 <.>
2025-04-28 23:46:01 +0800

    Update-在404頁面新增圖片

--- views/404.php
+++ views/404.php
@@ -1,6 +1,7 @@
 <?php require_once __DIR__ . '/../header.php'; ?>
 
 <div class="container text-center my-auto">
+    <img src="images/title.png" alt="" class="img-fulid">
     <h1 class="display-1">404</h1>
     <p class="lead">找不到您要的頁面</p>
     <a href="index.php" class="btn btn-success mt-3">返回首頁</a>
